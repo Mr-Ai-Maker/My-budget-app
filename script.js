7ced15bdf222106e51d93f099c97ee37b351ad9f
Mr.Ai <ahmed253340@gmail.com>
2026-07-31 04:18:38 +0300

    Update script.js

--- script.js
+++ script.js
@@ -421,3 +421,13 @@ btn.classList.remove("active");
 event.target.classList.add("active");
 
 }
+
+function showPage(page){
+
+document.querySelectorAll(".page").forEach(p=>{
+p.classList.remove("active");
+});
+
+document.getElementById(page).classList.add("active");
+
+}
